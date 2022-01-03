7c067a3ce5e40fcb64761436385d258488186d04
samswaringen <samswaringen@gmail.com>
2022-01-03 15:33:29 -0500

    idk

--- src/components/ClickUpButton.js
+++ src/components/ClickUpButton.js
@@ -2,7 +2,6 @@ import React from 'react'
 
 function ClickUpButton(props) {
     const {ticket} = props
-
     const getClickUp=()=>{
         console.log("click up ticket associated is", ticket.custom_fields.cf_clickup_ticket)
     }
